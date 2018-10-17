86e7870d84ca12fb24f0c584614fd24452bec544
Gaurav Kandlikar <gaurav.kandlikar@gmail.com>
2018-10-16 20:27:09 -0700

    fixes per eds request

--- make-figures.R
+++ make-figures.R
@@ -12,9 +12,9 @@ psobj <- convert_anacapa_to_phyloseq(taxDat, metDat)
 # Figure 1: Rarefying/Sp accumulation curve --------
 psobj <- subset_samples(psobj, subset = (Sample != "PCR_blank_3"))
 psobj <- subset_samples(psobj, subset = (Sample != "PCR_blank_4"))
-fig1 <- ggrare(psobj, step = 1000, se = FALSE, color = "Sample") + 
+fig1_static <- ggrare(psobj, step = 1000, se = FALSE, color = "Sample") + 
   theme_ranacapa()
-fig1 <- ggplotly(fig1, tooltip = c("Sample"))
+fig1 <- ggplotly(fig1_static, tooltip = c("Sample"))
 
 # Figure 2: Taxonomy heatmap ----------
 
@@ -48,24 +48,24 @@ fig2 <- heatmaply(tax_table, Rowv = F, Colv = F, hide_colorbar = F,
 # Figure 3: Taxonomy barplot ---------
 
 psobj_glommed <- tax_glom(psobj, "Order")
-fig3 <- plot_bar(psobj_glommed, fill = "Order") + theme_ranacapa() +
+fig3_static <- plot_bar(psobj_glommed, fill = "Order") + theme_ranacapa() +
   theme(axis.text.x = element_text(angle = 45)) +
-  theme(axis.title = element_blank())
+  theme(axis.title = element_blank()) 
 
-fig3 <- ggplotly() %>%
+fig3 <- ggplotly(fig3_static) %>%
   layout(yaxis = list(title = "Abundance", titlefont = list(size = 16)),
          xaxis = list(title = "Sample", titlefont = list(size = 16)),
          margin = list(l = 70, b = 100)) 
 
 # Figure 4: Alpha diversity plots --------
 psobj_island <- subset_samples(psobj, !is.na(Island))
-plot_richness(psobj_island, measures = "Shannon", color = "Island", x = "Island") + 
+fig4_static <- plot_richness(psobj_island, measures = "Shannon", color = "Island", x = "Island") + 
   theme_ranacapa() +
   geom_boxplot(aes_string(fill = "Island", alpha = 0.2)) + 
   theme(legend.position = "none") +
   theme(axis.title = element_blank()) +
   theme(axis.text.x = element_text(angle = 45))
-fig4 <- ggplotly(tooltip = c("x", "value")) %>%
+fig4 <- ggplotly(fig4_static, tooltip = c("x", "value")) %>%
   layout(yaxis = list(title = paste("Shannon Diversity"), titlefont = list(size = 16)),
          xaxis = list(title = "Island", titlefont = list(size = 16)),
          margin = list(l = 60, b = 70))
@@ -74,16 +74,21 @@ fig4 <- ggplotly(tooltip = c("x", "value")) %>%
 
 d <- distance(psobj_island, method= "jaccard")
 ord <- ordinate(psobj_island, method = "MDS", distance = d)
-nmdsplot <- plot_ordination(psobj_island, ord,
+fig5_static <- plot_ordination(psobj_island, ord,
                             color = "Island", shape = "Island") +
   ggtitle(paste("Island PCoA; dissimilarity method:",
                 tools::toTitleCase("Jaccard dissimilarity"))) +
   theme(plot.title = element_text(hjust = 0.5)) +
   theme_ranacapa()
-fig5 <- ggplotly(tooltip = c("Island", "x", "y")) %>%
+fig5 <- ggplotly(fig5_static, tooltip = c("Island", "x", "y")) %>%
   layout(hovermode = 'closest')
 
-save.image(file = "make-figures.Rdata")
+
+fig1 <- config(fig1, showLink=TRUE)
+fig2 <- config(fig2, showLink=TRUE)
+fig3 <- config(fig3, showLink=TRUE)
+fig4 <- config(fig4, showLink=TRUE)
+fig5 <- config(fig5, showLink=TRUE)
 
 # ----------
 htmlwidgets::saveWidget(fig1, file = file.path(getwd(), "figure-files", "fig1.html"), selfcontained = T)
@@ -91,3 +96,6 @@ htmlwidgets::saveWidget(fig2, file = file.path(getwd(), "figure-files", "fig2.ht
 htmlwidgets::saveWidget(fig3, file = file.path(getwd(), "figure-files", "fig3.html"), selfcontained = T)
 htmlwidgets::saveWidget(fig4, file = file.path(getwd(), "figure-files", "fig4.html"), selfcontained = T)
 htmlwidgets::saveWidget(fig5, file = file.path(getwd(), "figure-files", "fig5.html"), selfcontained = T)
+ggsave(filename = "Kandlikar-etal-figures-static/fig1.tiff",
+       )
+save.image(file = "make-figures.Rdata")
